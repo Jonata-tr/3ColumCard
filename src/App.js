fd886858dfd13961d704793cd464b3001405cf55
Jonatan Trindade <109476031+Jonata-tr@users.noreply.github.com>
2022-11-23 19:15:25 -0300

    Inicio do desevenvolvimento do projeto

---
+++ src/App.js
@@ -0,0 +1,78 @@
+import sedan from "./images/icon-sedans.svg"
+import luxury from "./images/icon-luxury.svg"
+import suvs from "./images/icon-suvs.svg"
+import "./App.css"
+
+const veiculos = [
+  { id: 'sedan',
+    details: [
+      {
+        nome: 'SEDANS',
+        img: sedan,
+        texto: 'Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on you next road trip.',
+        button: 'Learn More'
+      }
+    ]
+  },
+  { id: 'suvs',
+    details: [
+      {
+        nome: 'SUVS',
+        img: suvs,
+        texto: 'Take an SUV for its spacious interios, power, and versatility.Perfect for your next family vacation and off-road adventures.',
+        button: 'Learn More'
+      }
+    ]
+  },
+  { id: 'luxury',
+    details: [
+      {
+        nome: 'LUXURY',
+        img: luxury,
+        texto: 'Cruise in the best car brands whitout the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.',
+        button: 'Learn More'
+      }
+    ]
+  }
+]
+
+function Main() {
+  return(
+    <main>
+      {veiculos.map(cars => (
+        <Cars key={cars.id} id={cars.id} details={cars.details}/>
+      ))}
+    </main>
+  )
+}
+
+function Cars({id, details}) {
+  
+  return(
+    <section>
+      {details.map(det => (
+        <div className={id + " setores"}>
+
+          <div className="img">
+            <img src={det.img}/>
+          </div>
+
+          <div className="titulo">
+            <h1>{det.nome}</h1>
+          </div> 
+
+          <div className="texto">
+            <p>{det.texto}</p>
+          </div>
+
+          <div>
+            <button>{det.button}</button>
+          </div>
+          
+        </div>
+     ))}
+    </section>
+  )
+}
+
+export default Main
