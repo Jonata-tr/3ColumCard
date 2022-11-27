f639e16934f8278020e50d402e7c03892b4e9866
Jonatan Trindade <109476031+Jonata-tr@users.noreply.github.com>
2022-11-27 20:10:07 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,66 @@
+# Frontend Mentor - 3-column preview card component solution
+
+This is a solution to the [3-column preview card component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/3column-preview-card-component-pH92eAR2-). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout depending on their device's screen size
+- See hover states for interactive elements
+
+### Screenshot
+
+![](./src/design/desktop-design.jpg)
+![](./src/design/mobile-design.jpg)
+
+### Links
+
+- Solution URL: 
+- Live Site URL: 
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- [React](https://reactjs.org/) - JS library
+
+### What I learned
+
+Learned a lot about React flow during the process of writing the code.
+
+I'm really happy with this little bit of code:
+
+```jsx
+const [colorB, setColor] = useState(false)
+
+<div>
+  <button 
+  style={{
+    color: colorB === true ? "#fff" : det.buttonText,
+    backgroundColor: colorB === true ? det.buttonText: ""
+  }
+  }
+  onMouseEnter={() => {setColor(true)}}
+  onMouseOut={() => {setColor(false)}}  
+  >Learn More</button>
+</div>
+```
+
--- src/App.js
+++ src/App.js
@@ -1,82 +1,95 @@
-import sedan from "./images/icon-sedans.svg"
-import luxury from "./images/icon-luxury.svg"
-import suvs from "./images/icon-suvs.svg"
-import "./App.css"
+import {useState} from "react";
+import sedan from "./images/icon-sedans.svg";
+import luxury from "./images/icon-luxury.svg";
+import suvs from "./images/icon-suvs.svg";
+import "./App.css";
 
 const veiculos = [
-  { id: 'sedan',
+  {
+    id: "sedan",
     details: [
       {
-        nome: 'SEDANS',
+        nome: "SEDANS",
         img: sedan,
-        texto: 'Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on you next road trip.',
-        button: 'Learn More',
-        buttonText: "hsl(31, 77%, 52%)"
-      }
-    ]
+        texto:
+          "Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on you next road trip.",
+        button: "Learn More",
+        buttonText: "hsl(31, 77%, 52%)",
+      },
+    ],
   },
-  { id: 'suvs',
+  {
+    id: "suvs",
     details: [
       {
-        nome: 'SUVS',
+        nome: "SUVS",
         img: suvs,
-        texto: 'Take an SUV for its spacious interios, power, and versatility. Perfect for your next family vacation and off-road adventures.',
-        button: 'Learn More',
-        buttonText: "hsl(184, 100%, 22%)"
-      }
-    ]
+        texto:
+          "Take an SUV for its spacious interios, power, and versatility. Perfect for your next family vacation and off-road adventures.",
+        button: "Learn More",
+        buttonText: "hsl(184, 100%, 22%)",
+      },
+    ],
   },
-  { id: 'luxury',
+  {
+    id: "luxury",
     details: [
       {
-        nome: 'LUXURY',
+        nome: "LUXURY",
         img: luxury,
-        texto: 'Cruise in the best car brands whitout the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.',
-        button: 'Learn More',
-        buttonText: "hsl(179, 100%, 13%)"
-
-      }
-    ]
-  }
-]
+        texto:
+          "Cruise in the best car brands whitout the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.",
+        button: "Learn More",
+        buttonText: "hsl(179, 100%, 13%)",
+      },
+    ],
+  },
+];
 
 function Main() {
-  return(
+  return (
     <main>
-      {veiculos.map(cars => (
-        <Cars key={cars.id} id={cars.id} details={cars.details}/>
+      {veiculos.map((cars) => (
+        <Cars key={cars.id} id={cars.id} details={cars.details} />
       ))}
     </main>
-  )
+  );
 }
 
-function Cars({id, details}) {
-  
-  return(
-    <section>
-      {details.map(det => (
-        <div className={id + " setores"}>
+function Cars({ id, details }) {
+  const [colorB, setColor] = useState(false)
 
+  return (
+    <section className={id + " setores"}>
+      {details.map((det) => (
+        <div className="flex">
           <div className="img">
-            <img src={det.img}/>
+            <img src={det.img} />
           </div>
 
           <div className="titulo">
             <h1>{det.nome}</h1>
-          </div> 
+          </div>
 
           <div className="texto">
             <p>{det.texto}</p>
           </div>
 
           <div>
-            <button>Learn More</button>
+            <button 
+            style={{
+              color: colorB === true ? "#fff" : det.buttonText,
+              backgroundColor: colorB === true ? det.buttonText: ""
+            }
+            }
+            onMouseEnter={() => {setColor(true)}}
+            onMouseOut={() => {setColor(false)}}  
+            >Learn More</button>
           </div>
-          
         </div>
-     ))}
+      ))}
     </section>
-  )
+  );
 }
 
-export default Main
+export default Main;
